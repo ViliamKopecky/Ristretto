7b185481ecdb071097c7cd8e6f23e139c2afeee2
ViliamKopecky <vk@viliamkopecky.cz>
2013-05-26 21:15:45 +0200

    Autoreconnecting client-side script

--- client/ristretto.js
+++ client/ristretto.js
@@ -2,25 +2,28 @@
 	if(!window.io) {
 		console.warn('You shall turn Ristretto on.');
 	} else {
-		var url = location.protocol+'//'+location.hostname+":<%= port %>/";
-		var socket = io.connect(url);
-		socket.on("error", function(e){
-			console.warn('error ', arguments);
-		})
-		socket.on("connect", function() {
-			console.log('Ristretto on.');
-			socket.on("reload stylesheets", function() {
-				var links = document.getElementsByTagName("link");
-				for(var i = 0; i < links.length; i++) {
-					var link = links[i];
-					if(link.rel === "stylesheet") {
-						link.href += "?";
+		var connect_ristretto = function() {
+			var url = location.protocol+'//'+location.hostname+":<%= port %>/";
+			var socket = io.connect(url);
+			socket.on("error", function(){
+				console.warn('error', arguments);
+			});
+			socket.on("connect", function() {
+				console.log('Ristretto on.');
+				socket.on("reload stylesheets", function() {
+					var links = document.getElementsByTagName("link");
+					for(var i = 0; i < links.length; i++) {
+						var link = links[i];
+						if(link.rel === "stylesheet") {
+							link.href += "?";
+						}
 					}
-				}
+				});
+				socket.on("reload", function() {
+					window.location.reload();
+				});
 			});
-			socket.on("reload", function() {
-				window.location.reload();
-			});
-		});
+		};
+		connect_ristretto();
 	}
 })();
