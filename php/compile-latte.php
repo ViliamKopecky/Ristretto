d7a13a576d452f76249bf8028405e6bfdfd7122b
ViliamKopecky <vk@viliamkopecky.cz>
2013-05-26 21:17:35 +0200

    Typo in comment

--- php/compile-latte.php
+++ php/compile-latte.php
@@ -3,7 +3,7 @@
 /**
  * Latte compiler entry point
  *
- * Use: `php compile-latte.php ../path/to/template.latte "{'model':['../model/*']}"`
+ * Use: `php compile-latte.php ../path/to/template.latte "{'model_dir':['../model/*']}"`
  */
 
 require __DIR__ . '/app/bootstrap.php';
