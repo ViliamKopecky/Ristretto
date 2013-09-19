a036b949ca8157b3b798edee389b73e89378dcd9
ViliamKopecky <vk@viliamkopecky.cz>
2013-09-19 22:02:12 +0200

    First page load forces non-cached stylesheets

--- client/ristretto.js
+++ client/ristretto.js
@@ -4,25 +4,27 @@
 	} else {
 		var connect_ristretto = function() {
 			var url = location.protocol+'//'+location.hostname+":<%= port %>/";
+			var reload_stylesheets = function(first_time) {
+				var links = document.getElementsByTagName("link");
+				for(var i = 0; i < links.length; i++) {
+					var link = links[i];
+					if(link.rel === "stylesheet") {
+						link.href += (first_time === true) ? ("?"+(new Date()).getTime()) : "?";
+					}
+				}
+			};
 			var socket = io.connect(url);
 			socket.on("error", function(){
 				console.warn('error', arguments);
 			});
 			socket.on("connect", function() {
 				console.log('Ristretto on.');
-				socket.on("reload stylesheets", function() {
-					var links = document.getElementsByTagName("link");
-					for(var i = 0; i < links.length; i++) {
-						var link = links[i];
-						if(link.rel === "stylesheet") {
-							link.href += "?";
-						}
-					}
-				});
+				socket.on("reload stylesheets", reload_stylesheets);
 				socket.on("reload", function() {
 					window.location.reload();
 				});
 			});
+			reload_stylesheets(true);
 		};
 		connect_ristretto();
 	}
